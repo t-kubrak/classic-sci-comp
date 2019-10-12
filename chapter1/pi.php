c596cf43d1574eb53668311670b2e9a94d89ebe9
Taras <chikashikaari@gmail.com>
2019-10-12 14:57:20 -0400

    Pi - fix formatting

--- chapter1/pi.php
+++ chapter1/pi.php
@@ -1,11 +1,12 @@
 <?php
-function calculatePi(int $nTerms): float {
+function calculatePi(int $nTerms): float
+{
     $numerator = 4.0;
     $denominator = 1.0;
     $operation = 1;
     $pi = 0.0;
-    
-    for($counter = 0; $counter <= $nTerms; $counter++) {
+
+    for ($counter = 0; $counter <= $nTerms; $counter++) {
         $pi += $operation * ($numerator / $denominator);
         $denominator += 2;
         $operation *= -1;
