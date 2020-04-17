66a23fac7a2c1f42c5dbbeab193e84d942a6d5e8
t-kubrak <chikashikaari@gmail.com>
2020-04-17 16:13:58 -0400

    feat: word search; generate domain

--- chapter3/word_search.php
+++ chapter3/word_search.php
@@ -21,7 +21,7 @@ class GridLocation
     }
 }
 
-function generateGrid(int $rows, int $columns):array
+function generateGrid(int $rows, int $columns): array
 {
     $characters = range('A', 'Z');
     $grid = [];
@@ -40,4 +40,57 @@ function displayGrid(array $grid): void
     foreach ($grid as $row) {
         echo implode("", $row);
     }
+}
+
+function generateDomain(string $word, array $grid)
+{
+    $domain = TypedSequence::forType('array');
+    $height = count($grid);
+    $width = count($grid[0]);
+    $length = strlen($word);
+
+    foreach (range(0, $height) as $row) {
+        foreach (range(0, $width) as $col) {
+            $columns = range($col, $col + $length + 1);
+            $rows = range($row, $row + $length + 1);
+
+            if ($col + $length <= $width) {
+                /** left to right */
+                $domain->append(
+                    array_map(function ($c) use ($row) {
+                        return new GridLocation($row, $c);
+                    }, $columns)
+                );
+
+                /** diagonal towards bottom right */
+                if ($row + $length <= $height) {
+                    $domain->append(
+                        array_map(function ($r) use ($row, $col) {
+                            return new GridLocation($r, $col + ($r - $row));
+                        }, $rows)
+                    );
+                }
+            }
+
+            if ($row + $length <= $height) {
+                /** top to bottom */
+                $domain->append(
+                    array_map(function ($r) use ($col) {
+                        return new GridLocation($r, $col);
+                    }, $rows)
+                );
+
+                /** diagonal towards bottom left */
+                if ($col - $length >= 0) {
+                    $domain->append(
+                        array_map(function ($r) use ($row, $col) {
+                            return new GridLocation($r, $col - ($r - $row));
+                        }, $rows)
+                    );
+                }
+            }
+        }
+    }
+
+    return $domain;
 }
