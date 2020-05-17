f2dc7d8ea3e27056f4c0be6a129acdd7164f289e
t-kubrak <chikashikaari@gmail.com>
2020-05-17 17:20:54 -0400

    feat: generate segments

--- chapter8/connectfour.php
+++ chapter8/connectfour.php
@@ -38,3 +38,61 @@ class C4Piece implements Piece
     }
 }
 
+function generateSegments(int $numColumns, int $numRows, int $segmentLength): Sequence
+{
+    $segments = new Sequence();
+
+    // generate vertical segments
+    foreach (range(0, $numColumns - 1) as $c) {
+        foreach (range(0, $numRows - $segmentLength) as $r) {
+            $segment = new Sequence();
+
+            foreach (range(0, $segmentLength - 1) as $t) {
+                $segment->append(new Sequence([$c, $r + $t]));
+            }
+
+            $segments->append($segment);
+        }
+    }
+
+    // generate horizontal segments
+    foreach (range(0, $numColumns - $segmentLength) as $c) {
+        foreach (range(0, $numRows - 1) as $r) {
+            $segment = new Sequence();
+
+            foreach (range(0, $segmentLength - 1) as $t) {
+                $segment->append(new Sequence([$c + $t, $r]));
+            }
+
+            $segments->append($segment);
+        }
+    }
+
+    // generate the bottom left to top right diagonal segments
+    foreach (range(0, $numColumns - $segmentLength) as $c) {
+        foreach (range(0, $numRows - $segmentLength) as $r) {
+            $segment = new Sequence();
+
+            foreach (range(0, $segmentLength - 1) as $t) {
+                $segment->append(new Sequence([$c + $t, $r + $t]));
+            }
+
+            $segments->append($segment);
+        }
+    }
+
+    // generate the top left to bottom right diagonal segments
+    foreach (range(0, $numColumns - $segmentLength) as $c) {
+        foreach (range($segmentLength - 2, $numRows) as $r) {
+            $segment = new Sequence();
+
+            foreach (range(0, $segmentLength - 1) as $t) {
+                $segment->append(new Sequence([$c + $t, $r - $t]));
+            }
+
+            $segments->append($segment);
+        }
+    }
+
+    return $segments;
+}
