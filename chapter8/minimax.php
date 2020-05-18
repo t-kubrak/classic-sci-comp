1d64f6ab2a7d2ba5b1b3d0069da4204d4d3a9bbd
t-kubrak <chikashikaari@gmail.com>
2020-05-17 20:58:26 -0400

    feat: alphabeta pruning

--- chapter8/minimax.php
+++ chapter8/minimax.php
@@ -30,13 +30,69 @@ function minimax(Board $board, bool $maximizing, Piece $originalPlayer, int $max
     }
 }
 
+function alphabeta(
+    Board $board,
+    bool $maximizing,
+    Piece $originalPlayer,
+    int $maxDepth = 8,
+    float $alpha = -INF,
+    float $beta = INF
+): float
+{
+    // Base case – terminal position or maximum depth reached
+    if ($board->isWin() || $board->isDraw() || $maxDepth == 0) {
+        return $board->evaluate($originalPlayer);
+    }
+
+    // Recursive case - maximize your gains or minimize the opponent's gains
+    if ($maximizing) {
+        foreach ($board->legalMoves() as $move) {
+            $result = alphabeta(
+                $board->move($move),
+                false,
+                $originalPlayer,
+                $maxDepth - 1,
+                $alpha,
+                $beta
+            );
+
+            $alpha = max($result, $alpha);
+
+            if ($beta <= $alpha) {
+                break;
+            }
+        }
+
+        return $alpha;
+    } else {
+        foreach ($board->legalMoves() as $move) {
+            $result = alphabeta(
+                $board->move($move),
+                true,
+                $originalPlayer,
+                $maxDepth - 1,
+                $alpha,
+                $beta
+            );
+
+            $beta = min($result, $beta);
+
+            if ($beta <= $alpha) {
+                break;
+            }
+        }
+
+        return $beta;
+    }
+}
+
 function findBestMove(Board $board, int $maxDepth = 8): Move
 {
     $bestEval = -INF;
     $bestMove = new Move(-1);
 
     foreach ($board->legalMoves() as $move) {
-        $result = minimax($board->move($move), false, $board->turn(), $maxDepth);
+        $result = alphabeta($board->move($move), false, $board->turn(), $maxDepth);
 
         if ($result > $bestEval) {
             $bestEval = $result;
